42acf1e0c88d6e01c34dc7b398fd0b2a993f53df
RidzkyGuntur <2006024@itg.ac.id>
2023-04-11 10:53:51 +0700

    update

--- lib/modules/news_detail_screen/news_detail_screen.dart
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -1,58 +1,35 @@
 import 'package:flutter/material.dart';
 import 'news.dart';
 
-class NewsDetailScreen extends StatefulWidget {
-  const NewsDetailScreen({Key? key, required this.news}) : super(key: key);
+import 'package:flutter/material.dart';
 
-  final News news;
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    Key? key,
+    required this.id,
+    required this.content,
+    required this.imagePath,
+    required this.title,
+    this.news,
+  }) : super(key: key);
+
+  final String id;
+  final String content;
+  final String imagePath;
+  final String title;
+  final dynamic news;
 
   @override
   _NewsDetailScreenState createState() => _NewsDetailScreenState();
 }
 
 class _NewsDetailScreenState extends State<NewsDetailScreen> {
-  late News _news;
-
-  @override
-  void initState() {
-    super.initState();
-    _news = widget.news;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_news.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              _news.imagePath,
-              width: double.infinity,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                _news.title,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Text(
-                _news.content,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
+      body: Center(
+        child: Text(
+          widget.id,
         ),
       ),
     );
